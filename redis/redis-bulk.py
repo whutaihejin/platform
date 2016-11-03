c82b2e914f2f3e75f530c0d9b09a8924ab8acabd
taihejin <taihejin@bogon.didichuxing.com>
2016-11-03 18:17:38 +0800

    learning & fighting

--- redis/redis-bulk.py
+++ redis/redis-bulk.py
@@ -8,8 +8,10 @@ import getopt
 key = "self"
 limit = 7072
 batch = 100
+host_ip = "127.0.0.1"
+port = 6397
 
-opts, args = getopt.getopt(sys.argv[1:], "hl:b:k:", ["help", "limit=", "batch=", "key="])
+opts, args = getopt.getopt(sys.argv[1:], "l:b:k:h:p:", ["limit=", "batch=", "key=", "host=", "port="])
 for op, value in opts:
     if op == "-l" or  op == "--limit":
       limit = int(value)
@@ -17,20 +19,21 @@ for op, value in opts:
       batch = int(value)
     elif op == "-k" or op == "--key":
       key = value
-    elif op == "-h" or op == "--help":
-      print "-h --help"
-      print "-l --limit"
+    elif op == "-h" or op == "--host":
+      host = value
+    elif op == "-p" or op == "--port":
+      port = int(port)
 
+count = 0
 longitude = 79.0
 latitude = 19.0
 delta = 0.004474
-count = 0
 mount = limit * limit
 
 print limit, batch
 
 # redis client
-r = redis.Redis(host='10.94.106.180', port=6379, db=0)
+r = redis.Redis(host=host_ip, port=6379, db=0)
 #r = redis.StrictRedis(host='10.94.106.180', port=6379, db=0)
 
 start_time_ms = int(round(time.time() * 1000))
@@ -51,7 +54,6 @@ for x in range(0, limit):
       address = 'xx street, {:d} number, haidian, beijing'.format(count)
       value = (longitude, latitude, address)
       geo_list += value
-    print geo_list
     r.geoadd(key, *geo_list)
     print ("%.2f%% %d/%d" % ((count * 100.0 / mount), count, mount)), geo_list
   longitude += delta
