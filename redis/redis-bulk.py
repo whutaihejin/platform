c2e316474a38c6bbceccda85eaebd975ab91e2d4
taihejin <taihejin@bogon.didichuxing.com>
2016-11-03 18:12:08 +0800

    learning & fighting

--- redis/redis-bulk.py
+++ redis/redis-bulk.py
@@ -2,13 +2,32 @@
 
 import redis
 import time
+import sys
+import getopt
+
+key = "self"
+limit = 7072
+batch = 100
+
+opts, args = getopt.getopt(sys.argv[1:], "hl:b:k:", ["help", "limit=", "batch=", "key="])
+for op, value in opts:
+    if op == "-l" or  op == "--limit":
+      limit = int(value)
+    elif op == "-b" or op == "--batch":
+      batch = int(value)
+    elif op == "-k" or op == "--key":
+      key = value
+    elif op == "-h" or op == "--help":
+      print "-h --help"
+      print "-l --limit"
 
 longitude = 79.0
 latitude = 19.0
-limit = 7072
 delta = 0.004474
 count = 0
 mount = limit * limit
+
+print limit, batch
 
 # redis client
 r = redis.Redis(host='10.94.106.180', port=6379, db=0)
@@ -16,16 +35,25 @@ r = redis.Redis(host='10.94.106.180', port=6379, db=0)
 
 start_time_ms = int(round(time.time() * 1000))
 print ("start time %d ms" % start_time_ms)
+
 # bulk insert
 for x in range(0, limit):
   latitude = 10.0
-  for y in range(0, limit):
-    latitude += delta
-    count += 1
-    address = 'xxxx street, {:d} number, jimo, qingdao, shandong, china'.format(count)
-    value = (longitude, latitude, address)
-    r.geoadd("self", *value)
-    print ("%.2f%% %d/%d" % ((count * 100.0 / mount), count, mount)), ("(%.6f, %.6f, %s)" % (value[0], value[1], value[2]))
+  y = 0
+  while y < limit:
+    k = 0
+    geo_list = ()
+    while k < batch and y < limit:
+      latitude += delta
+      count += 1
+      k += 1
+      y += 1
+      address = 'xx street, {:d} number, haidian, beijing'.format(count)
+      value = (longitude, latitude, address)
+      geo_list += value
+    print geo_list
+    r.geoadd(key, *geo_list)
+    print ("%.2f%% %d/%d" % ((count * 100.0 / mount), count, mount)), geo_list
   longitude += delta
  
 end_time_ms = int(round(time.time() * 1000))
--- redis/test.py
+++ redis/test.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python
-from redis import Redis
-r = Redis()
+import sys
+import redis
+import getopt
+
+opts, args = getopt.getopt(sys.argv[1:], "hl:", ["help", "limit="])
+limit = 10
+for op, value in opts:
+  if op == "-l" or  op == "--limit":
+    limit = value
+  elif op == "-h" or op == "--help":
+    print "-h --help"
+    print "-l --limit"
+print limit
+r = redis.Redis()
 for i in dir(r):
   if i.startswith("geo"):
     print (i)
 
 address = 'xxxx street {:d} beijing china'.format(22)
 print address
+
+geo_list = []
+value = (12, 1, "121")
+value2 = (22, 33, "2233")
+geo_list.append(value)
+geo_list.append(value2)
+print geo_list
