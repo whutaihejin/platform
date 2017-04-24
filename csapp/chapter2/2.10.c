b9874e31eade2f35e81e353fa0a2eb1b0dabb7ca
taihejin <taihejin2008@126.com>
2017-04-24 11:13:41 +0800

    inplace swap & reverse

---
+++ csapp/chapter2/2.10.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void inplace_swap(int* x, int* y) {
+  *y = *x ^ *y;
+  *x = *x ^ *y;
+  *y = *x ^ *y;
+}
+
+void reverse_array(int a[], int cnt) {
+  int first, last;
+  for (first = 0, last = cnt - 1;
+      first <= last;
+      first++, last--) {
+    inplace_swap(&a[first], &a[last]);
+  }
+}
+
+void show_array(int a[], int cnt) {
+  int i;
+  for (i = 0; i < cnt; ++i) {
+    printf("%d ", a[i]);
+  }
+  printf("\n");
+}
+
+int main(int argc, char* argv[]) {
+  int a[] = {1, 2, 3, 4};
+  reverse_array(a, 4);
+  show_array(a, 4);
+
+  int b[] = {1, 2, 3, 4, 5};
+  reverse_array(b, 5);
+  show_array(b, 5);
+}
