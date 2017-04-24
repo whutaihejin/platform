984360ee54bb9b4bf48e5b261d2d7fe7a9fdf1a3
taihejin <taihejin2008@126.com>
2017-04-24 11:14:44 +0800

    fix

--- csapp/chapter2/2.10.c
+++ csapp/chapter2/2.10.c
@@ -9,7 +9,7 @@ void inplace_swap(int* x, int* y) {
 void reverse_array(int a[], int cnt) {
   int first, last;
   for (first = 0, last = cnt - 1;
-      first <= last;
+      first < last;
       first++, last--) {
     inplace_swap(&a[first], &a[last]);
   }
